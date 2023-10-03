adfdc4b02226df02b3ff274780a429b6144ffed2
Strugariu Sergiu <sstrugariu.7@icloud.com>
2023-10-03 04:18:23 +0300

    removed

--- #framework/cfgs/connecting-cfg.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-
-Config.playerCount = 0
-Config.list = {}
-Config.steamIdentifier = nil
--- #framework/client/callbacks-client.lua
+++
@@ -1,8 +0,0 @@
-CreateThread(function()
-    while true do
-        Wait(0) 
-        if IsControlJustPressed(0, 38) then 
-            
-        end
-    end
-end)
